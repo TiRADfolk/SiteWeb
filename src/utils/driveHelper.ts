d297b47734989216fea1c4d5212406f7f1db9904
TiRADfolk <Tiradfolk@gmail.com>
2026-07-22 19:30:18 +0200

    Create driveHelper.ts

---
+++ src/utils/driveHelper.ts
@@ -0,0 +1,15 @@
+//src/utils/driveHelper.ts
+
+export function formatDriveImageUrl(url: string | undefined): string {
+  if (!url || typeof url !== 'string') return '';
+  
+  const driveRegex = /\/file\/d\/([^\/]+)/;
+  const match = url.match(driveRegex);
+  
+  if (match && match[1]) {
+    const fileId = match[1];
+    return `https://lh3.googleusercontent.com/d/$${fileId}=s1600`;
+  }
+  
+  return url;
+}
